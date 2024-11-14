cda9f23399f9a5eaa1d52c41f70366e7b11919a7
Muhammad Asif Khan <masfkhan417@gmail.com>
2024-11-14 23:30:29 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-Import required libraries
+# Import required libraries
 import streamlit as st
 import pandas as pd
 import torch
@@ -104,4 +104,3 @@ if st.button("Generate Response"):
             st.write(response)
     else:
         st.warning("Please enter at least one detail.")
-
